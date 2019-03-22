e47509f6dc8c3cf3772a5c2d92c625cca9a2a622
Aitaro Chaya <aitaro.chaya@gmail.com>
2019-03-23 04:02:07 +0900

    wol導入

--- hello.py
+++ hello.py
@@ -1,7 +1,29 @@
 # hello.py
-from flask import Flask
+from flask import Flask, request, jsonify, make_response
+from wakeonlan import send_magic_packet
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "¡Hola World!"
+
+@app.route("/hoge", methods=['GET'])
+def getHoge():
+    # URLパラメータ
+    params = request.args
+    response = {}
+    if 'param' in params:
+        response.setdefault('res', 'param is : ' + params.get('param'))
+    return make_response(jsonify(response))
+
+@app.route("/hoge", methods=['POST'])
+def postHoge():
+    # ボディ(application/json)パラメータ
+    params = request.json
+    print(params)
+    response = {}
+    if 'param' in params:
+        response.setdefault('res', 'param is : ' + params.get('param'))
+    return make_response(jsonify(response))
+
+app.run(host="127.0.0.1", port=5000)
