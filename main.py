4813da3baf66a8d1cfaf72c2832e74b77a7e1225
Aitaro Chaya <aitaro.chaya@gmail.com>
2019-04-02 07:04:12 +0900

    debug

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ def postHoge():
     # print('hogeghoggeg')
     # print(request.json)
     params = request.json
-    print(type(params))
+    print(type(unicode(params,'utf-8')))
     keywords = params.split(" ")
     # print(keywords)
     # print(type(params))
