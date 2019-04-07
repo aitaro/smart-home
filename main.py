ebed28fb2a37b9c7e6f6b0a51c9b3cfa1f7d4077
Aitaro Chaya <aitaro.chaya@gmail.com>
2019-04-07 16:37:04 +0900

    シャットダウン追加

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from flask import Flask, request, jsonify, make_response
 from wakeonlan import send_magic_packet
+import winrm
 
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_pyfile('application.cfg', silent=True)
@@ -18,21 +19,21 @@ def getHoge():
         response.setdefault('res', 'param is : ' + params.get('param'))
     return make_response(jsonify(response))
 
-@app.route("/hoge", methods=['POST'])
-def postHoge():
-    # ボディ(application/json)パラメータ
-    # print(request)
-    # print('hogeghoggeg')
-    # print(request.json)
-    params = request.json.encode('utf-8')
+@app.route("/pc", methods=['POST'])
+def postPc():
+    params = request.json #.encode('utf-8')
     # print(type(params.encode('utf-8')))
+    # import pdb; pdb.set_trace()
+    # print(params)
+    # print(type(params))
     keywords = params.split(" ")
     # print(keywords)
     # print(type(params))
     if 'つけ' in keywords:
         send_magic_packet(app.config['MAC_ADDRESS'])
     if '消し' in keywords:
-        print('shut down')
+        s = winrm.Session(app.config['DESKTOP_IP_ADDRESS'], auth=('aitaro', app.config['DESKTOP_LOGIN_PASSWORD']))
+        s.run_cmd('shutdown', ['/h'])
     response = {}
     if 'param' in params:
         response.setdefault('res', 'param is : ' + params.get('param'))
